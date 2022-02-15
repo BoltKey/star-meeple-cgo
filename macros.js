3d303abf90943d59d8fad23640d8896f4e9727a8
Adam Spanel <adam.spanel@seznam.com>
2022-02-15 22:44:24 +0100

    add take damage button

--- macros.js
+++ macros.js
@@ -1,13 +1,11 @@
-function evalMacro(macroName, options, gameSituation, eventManager) {
-  switch(macroName) {
-    case "takeDamage":
-      console.log("taking damage yay")
-      break;
-    case "takeArtifact":
-     
-      break;
-    case "takePirate":
-    
-      break;
-  }
+switch(macroName) {
+  case "takeDamage":
+    console.log("taking damage yay")
+    break;
+  case "takeArtifact":
+   
+    break;
+  case "takePirate":
+  
+    break;
 }
