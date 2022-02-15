9812ada79800a0b1823511359077387159c2b94a
Adam Spanel <adam.spanel@seznam.com>
2022-02-15 23:50:01 +0100

    add take damage button

--- macros.js
+++ macros.js
@@ -6,15 +6,15 @@ switch(macroName) {
     console.log(gameSituation, options);
     for (let player of gameSituation.players) {
       if (player.color == options.color) {
-        /*console.log("found player red");
+        console.log("found player red");
         for (let child of Object.values(player.children.unpositioned)) {
           if (child.name == "playerBoard") {
             for (let inventoryPos = 6; inventoryPos >= 0; --inventoryPos) {
               if (!child.children.inventory[inventoryPos]) {
                 eventManager.newEvent("game move", {
-                  child.id, 
-                  "inventory",
-                  inventoryPos,
+                  destinationId: child.id, 
+                  positionName: "inventory",
+                  positionIndex: inventoryPos,
                   type: "component reveal",
                   containerId: "idk"
                 })
@@ -22,7 +22,7 @@ switch(macroName) {
               
             }
           }
-        }*/
+        }
       }
     }
     break;
